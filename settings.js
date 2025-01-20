62cd86c719d90a1ab8b65bc9f2c0f1f369a4696f
ousmane2200 <odia87785@gmail.com>
2025-01-20 08:52:23 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
-global.bankname = "MONIEPOINT"
+global.bankname = "opye"
 
-global.banknumber = "8133729715"
+global.banknumber = "876266267"
 
-global.bankowner = "FADARE"
+global.bankowner = "patron"
 
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
